653c7d0e8b3126da4597bb178ab759fa82413ea5
Paula <cedones@gmail.com>
2021-02-26 23:25:10 -0300

    practicando

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  async function fetchData() {
+    const getData = await fetch(
+      "https://api.themoviedb.org/3/movie/popular?api_key=5214af6249bf105c4d2ab8ebc91d26f9"
+    );
+    const getJson = await getData.json();
+
+    console.log(getJson);
+    setData(getJson.results);
+  }
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Peliculas</h1>
+      <ul>
+        {data.map((item) => (
+          <li key={item.id}>{item.title}</li>
+        ))}
+      </ul>
+    </>
   );
 }
 
